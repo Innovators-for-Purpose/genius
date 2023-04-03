b4e1c2f8dfd0254a3cf9689328a232eceb845f62
Lee Kellogg <lkellogg@google.com>
2023-04-03 18:36:06 -0400

    Work from 4/3

---
+++ script.js
@@ -0,0 +1,64 @@
+function createComponent(templateId, template) {
+    const templateElement = document.createElement('template');
+    templateElement.setAttribute('id', templateId);
+    templateElement.innerHTML = template;
+    document.body.appendChild(templateElement);
+  
+    // in html:
+    //   <card-component></card-component>
+    customElements.define(templateId, class extends HTMLElement {
+      constructor() {
+        super();
+        const template = document.getElementById(templateId);
+        const shadowRoot = this.attachShadow({ mode: "open" });
+        shadowRoot.appendChild(template.content.cloneNode(true));
+  
+        // Apply external styles to the shadow DOM
+        const linkElem = document.createElement("link");
+        linkElem.setAttribute("rel", "stylesheet");
+        linkElem.setAttribute("href", "style-index.css");
+  
+        // Attach the created element to the shadow DOM
+        shadowRoot.appendChild(linkElem);
+      }
+    });
+  }
+
+createComponent('genius-card',`
+<div class="card-br-top genius-card">
+  <slot name="Genius Image"></slot>
+  <div class="card-content genius-card-content">
+    <div class="card-text">Spring 2023</div>
+    <div class="card-header"><slot name="Genius Name"></slot></div>
+    <button class="card-button">View <ion-icon name="arrow-forward-outline"></ion-icon></button>
+  </div>
+</div>
+`);
+
+// createComponent('card-component', `
+//     <div class="template-div">
+//         <h1 class="template-header">Header in template</h1>
+//         <p class="template-p"><slot name="paragraph-text"></slot></p>
+//     </div>
+// `);
+
+// createComponent('button-component', `
+//     <div class="template-div">
+//         <button>Click me</button>
+//     </div>
+// `);
+
+// Usage examples:
+{/*
+<card-component>
+  <div slot="paragraph-text">
+    <span>This is card 1</span>
+  </div>
+</card-component>
+<card-component>
+  <span slot="paragraph-text">This is card 2</span>
+</card-component>
+<button-component></button-component>
+<button-component></button-component>
+<button-component></button-component>
+*/}
